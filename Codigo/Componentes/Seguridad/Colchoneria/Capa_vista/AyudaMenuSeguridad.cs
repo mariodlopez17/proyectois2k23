112d831bc78bb7510418cafa3affdf8366b723c9
Edward8585 <110178067+Edward8585@users.noreply.github.com>
2023-03-18 21:04:52 -0600

    Variables Editadas

--- Codigo/Componentes/Seguridad/Colchoneria/Capa_vista/AyudaMenuSeguridad.cs
+++ Codigo/Componentes/Seguridad/Colchoneria/Capa_vista/AyudaMenuSeguridad.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,11 @@ namespace Vista_Seguridad
         public AyudaMenuSeguridad()
         {
             InitializeComponent();
+        }
+
+        private void pictureBoxMenuSeguridad_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
