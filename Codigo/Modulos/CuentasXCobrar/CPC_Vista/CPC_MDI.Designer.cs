ec4d3c7edafc73b9e958e154318305f8eb728943
ldavid94 <ldavid92394@gmail.com>
2023-04-15 20:14:23 -0600

    Mantenimiento de Mora

--- Codigo/Modulos/CuentasXCobrar/CPC_Vista/CPC_MDI.Designer.cs
+++ Codigo/Modulos/CuentasXCobrar/CPC_Vista/CPC_MDI.Designer.cs
@@ -38,12 +38,12 @@ namespace CPC_Vista
             this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cerrarSesiónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturasVencidasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.catalogoToolStripMenuItem,
@@ -54,71 +54,80 @@ namespace CPC_Vista
             this.cerrarSesiónToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1378, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(919, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // catalogoToolStripMenuItem
             // 
             this.catalogoToolStripMenuItem.Name = "catalogoToolStripMenuItem";
-            this.catalogoToolStripMenuItem.Size = new System.Drawing.Size(96, 29);
+            this.catalogoToolStripMenuItem.Size = new System.Drawing.Size(65, 22);
             this.catalogoToolStripMenuItem.Text = "Archivos";
             // 
             // catalogosToolStripMenuItem
             // 
             this.catalogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.monedaToolStripMenuItem,
-            this.tipoDePagosToolStripMenuItem});
+            this.tipoDePagosToolStripMenuItem,
+            this.facturasVencidasToolStripMenuItem});
             this.catalogosToolStripMenuItem.Name = "catalogosToolStripMenuItem";
-            this.catalogosToolStripMenuItem.Size = new System.Drawing.Size(108, 29);
+            this.catalogosToolStripMenuItem.Size = new System.Drawing.Size(72, 22);
             this.catalogosToolStripMenuItem.Text = "Catalogos";
             // 
             // monedaToolStripMenuItem
             // 
             this.monedaToolStripMenuItem.Name = "monedaToolStripMenuItem";
-            this.monedaToolStripMenuItem.Size = new System.Drawing.Size(227, 34);
+            this.monedaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.monedaToolStripMenuItem.Text = "Moneda";
             this.monedaToolStripMenuItem.Click += new System.EventHandler(this.monedaToolStripMenuItem_Click);
             // 
             // tipoDePagosToolStripMenuItem
             // 
             this.tipoDePagosToolStripMenuItem.Name = "tipoDePagosToolStripMenuItem";
-            this.tipoDePagosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.tipoDePagosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.tipoDePagosToolStripMenuItem.Text = "Tipo de Pagos";
             this.tipoDePagosToolStripMenuItem.Click += new System.EventHandler(this.tipoDePagosToolStripMenuItem_Click);
             // 
             // procesosToolStripMenuItem
             // 
             this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
-            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(99, 29);
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(66, 22);
             this.procesosToolStripMenuItem.Text = "Procesos";
             // 
             // herramientasToolStripMenuItem
             // 
             this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
-            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(133, 29);
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(90, 22);
             this.herramientasToolStripMenuItem.Text = "Herramientas";
             // 
             // ayudaToolStripMenuItem
             // 
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(79, 29);
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 22);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
             // 
             // cerrarSesiónToolStripMenuItem
             // 
             this.cerrarSesiónToolStripMenuItem.Name = "cerrarSesiónToolStripMenuItem";
-            this.cerrarSesiónToolStripMenuItem.Size = new System.Drawing.Size(132, 29);
+            this.cerrarSesiónToolStripMenuItem.Size = new System.Drawing.Size(88, 22);
             this.cerrarSesiónToolStripMenuItem.Text = "Cerrar Sesión";
+            // 
+            // facturasVencidasToolStripMenuItem
+            // 
+            this.facturasVencidasToolStripMenuItem.Name = "facturasVencidasToolStripMenuItem";
+            this.facturasVencidasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.facturasVencidasToolStripMenuItem.Text = "Facturas Vencidas";
             // 
             // CPC_MDI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1378, 1044);
+            this.ClientSize = new System.Drawing.Size(919, 679);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CPC_MDI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CPC_MDI";
@@ -140,5 +149,6 @@ namespace CPC_Vista
         private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cerrarSesiónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturasVencidasToolStripMenuItem;
     }
 }
---
+++ Codigo/Modulos/CuentasXCobrar/CPC_Vista/Mant_moras.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace CPC_Vista
+{
+    partial class Mant_moras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navegador1 = new NavegadorVista.Navegador();
+            this.tbl_moras = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_moras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(69, 2);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(574, 152);
+            this.navegador1.TabIndex = 1;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // tbl_moras
+            // 
+            this.tbl_moras.AllowUserToAddRows = false;
+            this.tbl_moras.AllowUserToDeleteRows = false;
+            this.tbl_moras.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tbl_moras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tbl_moras.Location = new System.Drawing.Point(235, 160);
+            this.tbl_moras.Name = "tbl_moras";
+            this.tbl_moras.ReadOnly = true;
+            this.tbl_moras.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.tbl_moras.Size = new System.Drawing.Size(442, 278);
+            this.tbl_moras.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 200);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(69, 197);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(161, 20);
+            this.txt_id.TabIndex = 6;
+            this.txt_id.Tag = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 241);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "CLIENTE";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(69, 238);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(161, 20);
+            this.txt_nombre.TabIndex = 8;
+            this.txt_nombre.Tag = "nombre";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(115, 282);
+            this.txtMonto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(115, 20);
+            this.txtMonto.TabIndex = 9;
+            this.txtMonto.Tag = "nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 285);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "MONTO FACTURA";
+            // 
+            // Mant_moras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 451);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbl_moras);
+            this.Controls.Add(this.navegador1);
+            this.Name = "Mant_moras";
+            this.Text = "Mant_moras";
+            ((System.ComponentModel.ISupportInitialize)(this.tbl_moras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NavegadorVista.Navegador navegador1;
+        private System.Windows.Forms.DataGridView tbl_moras;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.Label label3;
+    }
+}
