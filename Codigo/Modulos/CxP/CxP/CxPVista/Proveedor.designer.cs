d9d6f6335d0bdc34509bb151717037cb055c160c
LuisTorres2122 <ltorresb@miumg.edu.gt>
2023-05-05 18:38:45 -0600

    Correcion ambiguedad proveedor

--- Codigo/Modulos/CxP/CxP/CxPVista/Proveedor.designer.cs
+++ Codigo/Modulos/CxP/CxP/CxPVista/Proveedor.designer.cs
@@ -57,15 +57,6 @@ namespace CxPVista
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.navegador1 = new NavegadorVista.Navegador();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -73,8 +64,7 @@ namespace CxPVista
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label1.Location = new System.Drawing.Point(17, 173);
-            this.label1.Location = new System.Drawing.Point(43, 195);
+            this.label1.Location = new System.Drawing.Point(50, 169);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 17);
             this.label1.TabIndex = 1;
@@ -84,8 +74,7 @@ namespace CxPVista
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label2.Location = new System.Drawing.Point(17, 228);
-            this.label2.Location = new System.Drawing.Point(43, 234);
+            this.label2.Location = new System.Drawing.Point(48, 228);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 2;
@@ -95,8 +84,7 @@ namespace CxPVista
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label3.Location = new System.Drawing.Point(17, 258);
-            this.label3.Location = new System.Drawing.Point(43, 273);
+            this.label3.Location = new System.Drawing.Point(43, 257);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 17);
             this.label3.TabIndex = 3;
@@ -106,8 +94,7 @@ namespace CxPVista
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label4.Location = new System.Drawing.Point(17, 289);
-            this.label4.Location = new System.Drawing.Point(43, 319);
+            this.label4.Location = new System.Drawing.Point(48, 288);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 17);
             this.label4.TabIndex = 4;
@@ -117,12 +104,7 @@ namespace CxPVista
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label5.Location = new System.Drawing.Point(17, 316);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(148, 17);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Correo Electrónico";
-            this.label5.Location = new System.Drawing.Point(328, 211);
+            this.label5.Location = new System.Drawing.Point(48, 324);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(60, 17);
             this.label5.TabIndex = 5;
@@ -132,8 +114,7 @@ namespace CxPVista
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label6.Location = new System.Drawing.Point(17, 350);
-            this.label6.Location = new System.Drawing.Point(328, 255);
+            this.label6.Location = new System.Drawing.Point(48, 355);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 17);
             this.label6.TabIndex = 6;
@@ -143,8 +124,7 @@ namespace CxPVista
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label7.Location = new System.Drawing.Point(17, 381);
-            this.label7.Location = new System.Drawing.Point(328, 301);
+            this.label7.Location = new System.Drawing.Point(50, 386);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(58, 17);
             this.label7.TabIndex = 7;
@@ -153,7 +133,7 @@ namespace CxPVista
             // txtid
             // 
             this.txtid.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtid.Location = new System.Drawing.Point(65, 170);
+            this.txtid.Location = new System.Drawing.Point(102, 166);
             this.txtid.Name = "txtid";
             this.txtid.Size = new System.Drawing.Size(100, 25);
             this.txtid.TabIndex = 8;
@@ -212,74 +192,19 @@ namespace CxPVista
             this.txtestado.Size = new System.Drawing.Size(37, 25);
             this.txtestado.TabIndex = 14;
             this.txtestado.Tag = "estado_proveedor";
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox1.Location = new System.Drawing.Point(137, 192);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
-            this.textBox1.TabIndex = 8;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox2.Location = new System.Drawing.Point(137, 231);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 25);
-            this.textBox2.TabIndex = 9;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox3.Location = new System.Drawing.Point(137, 270);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(164, 25);
-            this.textBox3.TabIndex = 10;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox4.Location = new System.Drawing.Point(137, 316);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(138, 25);
-            this.textBox4.TabIndex = 11;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox5.Location = new System.Drawing.Point(406, 211);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(180, 25);
-            this.textBox5.TabIndex = 12;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox6.Location = new System.Drawing.Point(406, 252);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(180, 25);
-            this.textBox6.TabIndex = 13;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.textBox7.Location = new System.Drawing.Point(406, 298);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(37, 25);
-            this.textBox7.TabIndex = 14;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(20, 420);
+            this.dataGridView1.Location = new System.Drawing.Point(26, 409);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(934, 226);
+            this.dataGridView1.Size = new System.Drawing.Size(801, 226);
             this.dataGridView1.TabIndex = 15;
             this.dataGridView1.Tag = "tbl_proveedor";
             // 
             // navegador1
             // 
-            this.navegador1.Location = new System.Drawing.Point(144, 12);
+            this.navegador1.Location = new System.Drawing.Point(126, 3);
             this.navegador1.Name = "navegador1";
             this.navegador1.Size = new System.Drawing.Size(574, 152);
             this.navegador1.TabIndex = 16;
@@ -288,7 +213,7 @@ namespace CxPVista
             // txtcargo
             // 
             this.txtcargo.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtcargo.Location = new System.Drawing.Point(658, 268);
+            this.txtcargo.Location = new System.Drawing.Point(579, 258);
             this.txtcargo.Name = "txtcargo";
             this.txtcargo.Size = new System.Drawing.Size(138, 25);
             this.txtcargo.TabIndex = 17;
@@ -297,7 +222,7 @@ namespace CxPVista
             // txtcargoacum
             // 
             this.txtcargoacum.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtcargoacum.Location = new System.Drawing.Point(658, 344);
+            this.txtcargoacum.Location = new System.Drawing.Point(579, 334);
             this.txtcargoacum.Name = "txtcargoacum";
             this.txtcargoacum.Size = new System.Drawing.Size(138, 25);
             this.txtcargoacum.TabIndex = 18;
@@ -306,7 +231,7 @@ namespace CxPVista
             // txtabono
             // 
             this.txtabono.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtabono.Location = new System.Drawing.Point(658, 299);
+            this.txtabono.Location = new System.Drawing.Point(579, 289);
             this.txtabono.Name = "txtabono";
             this.txtabono.Size = new System.Drawing.Size(138, 25);
             this.txtabono.TabIndex = 19;
@@ -315,7 +240,7 @@ namespace CxPVista
             // txtsaldoactual
             // 
             this.txtsaldoactual.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtsaldoactual.Location = new System.Drawing.Point(658, 228);
+            this.txtsaldoactual.Location = new System.Drawing.Point(579, 218);
             this.txtsaldoactual.Name = "txtsaldoactual";
             this.txtsaldoactual.Size = new System.Drawing.Size(138, 25);
             this.txtsaldoactual.TabIndex = 20;
@@ -324,7 +249,7 @@ namespace CxPVista
             // txtsaldoanterior
             // 
             this.txtsaldoanterior.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtsaldoanterior.Location = new System.Drawing.Point(658, 197);
+            this.txtsaldoanterior.Location = new System.Drawing.Point(579, 187);
             this.txtsaldoanterior.Name = "txtsaldoanterior";
             this.txtsaldoanterior.Size = new System.Drawing.Size(138, 25);
             this.txtsaldoanterior.TabIndex = 21;
@@ -333,7 +258,7 @@ namespace CxPVista
             // txtabonoacum
             // 
             this.txtabonoacum.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.txtabonoacum.Location = new System.Drawing.Point(658, 375);
+            this.txtabonoacum.Location = new System.Drawing.Point(579, 365);
             this.txtabonoacum.Name = "txtabonoacum";
             this.txtabonoacum.Size = new System.Drawing.Size(138, 25);
             this.txtabonoacum.TabIndex = 22;
@@ -343,7 +268,7 @@ namespace CxPVista
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label8.Location = new System.Drawing.Point(480, 202);
+            this.label8.Location = new System.Drawing.Point(401, 192);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(172, 17);
             this.label8.TabIndex = 23;
@@ -353,7 +278,7 @@ namespace CxPVista
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label9.Location = new System.Drawing.Point(480, 231);
+            this.label9.Location = new System.Drawing.Point(401, 221);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(158, 17);
             this.label9.TabIndex = 24;
@@ -363,7 +288,7 @@ namespace CxPVista
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label10.Location = new System.Drawing.Point(480, 276);
+            this.label10.Location = new System.Drawing.Point(401, 266);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(113, 17);
             this.label10.TabIndex = 25;
@@ -373,7 +298,7 @@ namespace CxPVista
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label11.Location = new System.Drawing.Point(480, 307);
+            this.label11.Location = new System.Drawing.Point(401, 297);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(115, 17);
             this.label11.TabIndex = 26;
@@ -383,7 +308,7 @@ namespace CxPVista
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label12.Location = new System.Drawing.Point(480, 347);
+            this.label12.Location = new System.Drawing.Point(401, 337);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(137, 17);
             this.label12.TabIndex = 27;
@@ -393,29 +318,18 @@ namespace CxPVista
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F);
-            this.label13.Location = new System.Drawing.Point(480, 375);
+            this.label13.Location = new System.Drawing.Point(401, 365);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(139, 17);
             this.label13.TabIndex = 28;
             this.label13.Text = "Abono acumulado";
-            this.dataGridView1.Location = new System.Drawing.Point(39, 361);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(593, 226);
-            this.dataGridView1.TabIndex = 15;
-            // 
-            // navegador1
-            // 
-            this.navegador1.Location = new System.Drawing.Point(58, 12);
-            this.navegador1.Name = "navegador1";
-            this.navegador1.Size = new System.Drawing.Size(574, 152);
-            this.navegador1.TabIndex = 16;
             // 
             // Proveedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(93)))), ((int)(((byte)(86)))));
-            this.ClientSize = new System.Drawing.Size(966, 658);
+            this.ClientSize = new System.Drawing.Size(839, 647);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
@@ -428,8 +342,6 @@ namespace CxPVista
             this.Controls.Add(this.txtabono);
             this.Controls.Add(this.txtcargoacum);
             this.Controls.Add(this.txtcargo);
-            this.Controls.Add(this.navegador1);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.txtestado);
             this.Controls.Add(this.txtrubro);
             this.Controls.Add(this.txtcorreo);
@@ -437,16 +349,8 @@ namespace CxPVista
             this.Controls.Add(this.txtdireccion);
             this.Controls.Add(this.txtnombre);
             this.Controls.Add(this.txtid);
-            this.ClientSize = new System.Drawing.Size(659, 606);
             this.Controls.Add(this.navegador1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -492,14 +396,6 @@ namespace CxPVista
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private NavegadorVista.Navegador navegador1;
+   
     }
 }
