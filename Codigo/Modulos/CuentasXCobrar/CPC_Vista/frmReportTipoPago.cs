8aa1d058ae3abd8b09dd2ca484fcdcef8bbf4bc3
ldavid94 <ldavid92394@gmail.com>
2023-05-15 19:09:56 -0600

    Realizacion de Reportes de los Mantenimientos

---
+++ Codigo/Modulos/CuentasXCobrar/CPC_Vista/frmReportTipoPago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPC_Vista
+{
+    public partial class frmReportTipoPago : Form
+    {
+        public frmReportTipoPago()
+        {
+            InitializeComponent();
+        }
+    }
+}
