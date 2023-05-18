623d0f3c6e90d43b04bbc7d1906012a63560d920
LuisTorres2122 <ltorresb@miumg.edu.gt>
2023-05-17 21:46:18 -0600

    formulario de balance terminado

--- Codigo/Modulos/CxP/CxP/CxPVista/Balance.cs
+++ Codigo/Modulos/CxP/CxP/CxPVista/Balance.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CxPControlador;
 
 namespace CxPVista
 {
     public partial class Balance : Form
     {
+        TControlador controlador = new TControlador();
         public Balance()
         {
             InitializeComponent();
+        }
+
+        private void Balance_Load(object sender, EventArgs e)
+        {
+            controlador.llenardatosproveedor("tbl_proveedor", DgvProveedor);
+        }
+
+        private void txtProveedor_TextChanged(object sender, EventArgs e)
+        {
+            controlador.llenardatosproveedorfiltro("tbl_proveedor", DgvProveedor, "pk_id_proveedor", txtProveedor.Text);
+        }
+
+        private void iconButton2_Click(object sender, EventArgs e)
+        {
+            controlador.llenardatosproveedorfiltro("tbl_proveedor", DgvProveedor, "pk_id_proveedor", txtProveedor.Text);
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            controlador.balance(DgvProveedor);
         }
     }
 }
